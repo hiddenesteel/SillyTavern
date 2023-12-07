68df8428cde76c40a83c18993bbb89e0e66c0ec6
hiddenesteel <153188406+hiddenesteel@users.noreply.github.com>
2023-12-07 16:44:25 +0100

    .

--- pre-start.js
+++ pre-start.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
+const ejs = require('ejs');
+const yaml = require('js-yaml');
 const { exec } = require('child_process');
-
-
 
 console.log('-----------------')
 console.log('reading env var')
+
+const configTemplate = fs.readFileSync('config.yaml', 'utf8');
+const configString = ejs.render(configTemplate);
+
+try {
+    const config = yaml.load(configString, 'utf-8');
+    const yamlStr = yaml.dump(config);
+
+    fs.writeFileSync('config.yaml', yamlStr, 'utf8');
+} catch (e) {
+    console.error(e);
+}
 console.log('-----------------')
 
 console.log('-----------------')
 console.log('setting interval')
-setInterval(()=> {
+setInterval(() => {
     exec('pwd', (err, stdout, stderr) => {
         if (err) {
             //some err occurred
