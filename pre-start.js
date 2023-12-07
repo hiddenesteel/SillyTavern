f4c70564c083a4721ab507df0edf1b246c5a67c5
hiddenesteel <153188406+hiddenesteel@users.noreply.github.com>
2023-12-07 16:09:01 +0100

    .

--- pre-start.js
+++ pre-start.js
@@ -3,17 +3,7 @@ const { exec } = require('child_process');
 console.log('-----------------')
 console.log('setting interval')
 setInterval(()=> {
-    exec('echo "hello world"', (err, stdout, stderr) => {
-        if (err) {
-            //some err occurred
-            console.error(err)
-        } else {
-            // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-        }
-    });
-    exec('git status', (err, stdout, stderr) => {
+    exec('pwd', (err, stdout, stderr) => {
         if (err) {
             //some err occurred
             console.error(err)
